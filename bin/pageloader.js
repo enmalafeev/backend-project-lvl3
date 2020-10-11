d2a289e41da988d35e440628ebf7fe0fe02044d4
enmalafeev <malafeev.en@yandex.ru>
2020-10-11 13:35:08 +0300

    update pageloader.js

--- bin/pageloader.js
+++ bin/pageloader.js
@@ -1,3 +1,14 @@
 #!/usr/bin/env node
 
-console.log('Init project');
+import commander from 'commander';
+import pageloader from '../src/index.js';
+
+const programm = commander;
+
+programm
+  .version('0.0.1')
+  .description('Download page per url, and save in current directory')
+  .option('-o, --output [directory]', 'Output directory', `${process.cwd}`)
+  // .arguments('<url>')
+  .action((url) => console.log(pageloader(url)))
+  .parse(process.argv);

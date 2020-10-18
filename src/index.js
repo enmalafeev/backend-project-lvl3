3426467e41819180e4bddb9987a5a313e9fe4edb
enmalafeev <malafeev.en@yandex.ru>
2020-10-18 14:52:54 +0300

    add getStringNameFromURL function

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import axios from 'axios';
 import path from 'path';
 import { promises as fs } from 'fs';
 
-const pageloader = (url, pathToDir) => {
+const getStringNameFromURL = (url, ending) => {
   const { hostname, pathname } = new URL(url);
   const urlString = `${hostname}${pathname}`;
   const regex = /\W/gm;
-  const outputFileName = urlString.replace(regex, '-').concat('.html');
+  const outputName = urlString.replace(regex, '-').concat(ending);
+  return outputName;
+};
+
+const pageloader = (url, pathToDir) => {
+  const outputFileName = getStringNameFromURL(url, '.html');
 
   axios.get(url)
     .then((response) => response.data)
